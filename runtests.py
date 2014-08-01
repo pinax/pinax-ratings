07d2eafa73cc86667ed054175583aaacf5157cdd
Brian Rosner <brosner@gmail.com>
2014-07-31 22:14:46 -0600

    hooked up tests to Travis

---
+++ runtests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+
+from django.conf import settings
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+            }
+        },
+        ROOT_URLCONF="dialogos.urls",
+        INSTALLED_APPS=[
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.sessions",
+            "agon_ratings",
+        ],
+    )
+
+
+from django_nose import NoseTestSuiteRunner
+
+test_runner = NoseTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(["agon_ratings"])
+
+if failures:
+    sys.exit(failures)
