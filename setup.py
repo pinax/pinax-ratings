f13277567d7d713a0dd2baa652fafbfc02b7efd8
Patrick Altman <paltman@gmail.com>
2015-01-27 23:53:02 -0600

    Not ready for Python 3 yet

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+        # "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     zip_safe=False
